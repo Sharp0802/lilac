c66cde3d8ec99bea22b046e7e0acd73692361bb1
Sharp0802 <sharp0802.business@gmail.com>
2024-04-25 21:40:37 +0900

    Feat: Declare AST visitor for EnumDecl

---
+++ lilac-c/inc/enumvisitor.h
@@ -0,0 +1,33 @@
+#ifndef ENUMVISITOR_H
+#define ENUMVISITOR_H
+
+#include <clang/AST/RecursiveASTVisitor.h>
+
+#include "visitorbase.h"
+
+namespace lilac
+{
+    struct EnumInfo
+    {
+        clang::EnumDecl* Handle;
+        std::vector<clang::EnumConstantDecl*> Constant;
+    };
+
+    class EnumVisitor
+        : public clang::RecursiveASTVisitor<EnumVisitor>,
+          public VisitorBase<clang::EnumDecl, EnumInfo>
+    {
+        clang::EnumDecl* m_Enum;
+        std::vector<clang::EnumConstantDecl*> m_Constant;
+
+    public:
+        explicit EnumVisitor(clang::EnumDecl* decl);
+
+        bool VisitEnumConstantDecl(clang::EnumConstantDecl* constant);
+
+        [[nodiscard]]
+        EnumInfo GetInfo() const override;
+    };
+}
+
+#endif //ENUMVISITOR_H
