7e95a1f7c27ede64b0005b4731692d0aeed68adc
Sharp0802 <sharp0802.business@gmail.com>
2024-04-25 22:28:04 +0900

    Refactor: Move ASTVisitor to its own file

--- lilac-c/src/astconsumer.cxx
+++ lilac-c/src/astconsumer.cxx
@@ -2,60 +2,10 @@
 
 #include <clang/AST/RecursiveASTVisitor.h>
 
-#include "enumvisitor.h"
-#include "recordvisitor.h"
+#include "astvisitor.h"
 
 namespace lilac
 {
-    struct ASTInfo
-    {
-        std::vector<RecordInfo> Records;
-        std::vector<EnumInfo> Enums;
-        std::vector<clang::FunctionDecl*> Functions;
-    };
-
-    class ASTVisitor final
-            : public clang::RecursiveASTVisitor<ASTVisitor>,
-              public VisitorBase<ASTInfo>
-    {
-        std::vector<RecordInfo> m_Records;
-        std::vector<EnumInfo> m_Enums;
-        std::vector<clang::FunctionDecl*> m_Functions;
-
-    public:
-        bool VisitCXXRecordDecl(clang::RecordDecl* decl)
-        {
-            RecordVisitor visitor(decl);
-            visitor.TraverseDecl(decl);
-            m_Records.push_back(visitor.GetInfo());
-            return true;
-        }
-
-        bool VisitEnumDecl(clang::EnumDecl* decl)
-        {
-            EnumVisitor visitor(decl);
-            visitor.TraverseDecl(decl);
-            m_Enums.push_back(visitor.GetInfo());
-            return true;
-        }
-
-        bool VisitFunctionDecl(clang::FunctionDecl* decl)
-        {
-            if (IsVisibilityAllowed(decl))
-                m_Functions.push_back(decl);
-            return true;
-        }
-
-        [[nodiscard]]
-        ASTInfo GetInfo() const override
-        {
-            return {
-                m_Records,
-                m_Enums,
-                m_Functions
-            };
-        }
-    };
 
     ASTConsumer::ASTConsumer(ASTFrontendAction* action, clang::CompilerInstance& ci, llvm::StringRef file)
         : m_Action(action),
---
+++ lilac-c/src/astvisitor.cxx
@@ -0,0 +1,36 @@
+#include "astvisitor.h"
+
+namespace lilac
+{
+    bool ASTVisitor::VisitCXXRecordDecl(clang::RecordDecl* decl)
+    {
+        RecordVisitor visitor(decl);
+        visitor.TraverseDecl(decl);
+        m_Records.push_back(visitor.GetInfo());
+        return true;
+    }
+
+    bool ASTVisitor::VisitEnumDecl(clang::EnumDecl* decl)
+    {
+        EnumVisitor visitor(decl);
+        visitor.TraverseDecl(decl);
+        m_Enums.push_back(visitor.GetInfo());
+        return true;
+    }
+
+    bool ASTVisitor::VisitFunctionDecl(clang::FunctionDecl* decl)
+    {
+        if (IsVisibilityAllowed(decl))
+            m_Functions.push_back(decl);
+        return true;
+    }
+
+    ASTInfo ASTVisitor::GetInfo() const
+    {
+        return {
+            m_Records,
+            m_Enums,
+            m_Functions
+        };
+    }
+}
