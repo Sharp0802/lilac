945f0718f7fa5184a92672338d954d3a4aafa05a
Sharp0802 <sharp0802.business@gmail.com>
2024-04-25 21:40:18 +0900

    Feat: Implement AST visitor for CXXRecordDecl

---
+++ lilac-c/src/recordvisitor.cxx
@@ -0,0 +1,45 @@
+#include "recordvisitor.h"
+
+namespace lilac
+{
+    RecordVisitor::RecordVisitor(clang::RecordDecl* record): VisitorBase(record), m_Record(record)
+    {
+    }
+
+    bool RecordVisitor::VisitMSPropertyDecl(clang::MSPropertyDecl* property)
+    {
+        if (!IsVisibilityAllowed(property)) return true;
+
+        if (!property->hasGetter() && !property->hasSetter())
+        {
+            llvm::errs()
+                    << "A property '"
+                    << m_Record->getName() << "::" << property->getNameAsString()
+                    << "' with neither setter and getter is not allowed\n";
+            return false;
+        }
+
+        m_Properties.push_back(property);
+        llvm::outs() << m_Record->getName() << "::" << property->getName() << '\n';
+        return true;
+    }
+
+    bool RecordVisitor::VisitCXXMethodDecl(clang::CXXMethodDecl* method)
+    {
+        if (IsVisibilityAllowed(method))
+        {
+            m_Methods.push_back(method);
+            llvm::outs() << m_Record->getName() << "::" << method->getName() << '\n';
+        }
+        return true;
+    }
+
+    RecordInfo RecordVisitor::GetInfo() const
+    {
+        return {
+            m_Record,
+            m_Properties,
+            m_Methods
+        };
+    }
+}
