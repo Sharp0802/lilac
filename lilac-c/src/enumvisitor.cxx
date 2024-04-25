eb95b5d9e140fe5fb83533e696e35b5ca80dee99
Sharp0802 <sharp0802.business@gmail.com>
2024-04-25 21:41:22 +0900

    Feat: Implement AST visitor for EnumDecl

---
+++ lilac-c/src/enumvisitor.cxx
@@ -0,0 +1,26 @@
+#include "enumvisitor.h"
+
+namespace lilac
+{
+    EnumVisitor::EnumVisitor(clang::EnumDecl* decl): VisitorBase(decl), m_Enum(decl)
+    {
+    }
+
+    bool EnumVisitor::VisitEnumConstantDecl(clang::EnumConstantDecl* constant)
+    {
+        if (IsVisibilityAllowed(constant))
+        {
+            m_Constant.push_back(constant);
+            llvm::outs() << m_Enum->getName() << "::" << constant->getName() << '\n';
+        }
+        return true;
+    }
+
+    EnumInfo EnumVisitor::GetInfo() const
+    {
+        return {
+            m_Enum,
+            m_Constant
+        };
+    }
+}
