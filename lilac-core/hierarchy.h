c43bba1ff4daf7ac469b9bbfc7f48c05e6fdd2fe
Sharp0802 <sharp0802.business@gmail.com>
2024-05-13 17:39:56 +0900

    Feat: Create intermediate-hierarchy-representation model

---
+++ lilac-core/hierarchy.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <cstdint>
+#include <set>
+#include <string>
+
+namespace lilac::core
+{
+    enum HierarchyKind
+    {
+        HOK_Root,
+        HOK_Namespace,
+
+        HOK_Type,
+        HOK_Constant,
+
+        HOK_Function,
+        HOK_Method,
+
+        HOK_Property,
+        HOK_Getter,
+        HOK_Setter
+    };
+
+    struct ConstantData
+    {
+        bool Signed;
+        union
+        {
+            uint64_t ConstantUnsigned;
+            int64_t ConstantSigned;
+        };
+    };
+
+    struct Hierarchy
+    {
+    private:
+        void ToString(std::stringstream& ss, std::size_t indent) const;
+
+    public:
+        const std::string ActualName;
+        const std::string Name;
+
+        const HierarchyKind Kind;
+        std::set<Hierarchy> Members;
+
+        union
+        {
+            ConstantData Constant;
+        };
+
+        Hierarchy(HierarchyKind kind, std::string  actualName, std::string name);
+
+        [[nodiscard]]
+        std::string ToString() const;
+
+        bool operator==(const Hierarchy&) const;
+        bool operator!=(const Hierarchy&) const;
+
+        bool operator<(const Hierarchy&) const;
+    };
+}
---
+++ lilac-core/src/hierarchy.cxx
@@ -0,0 +1,65 @@
+#include <utility>
+#include <sstream>
+
+#include "hierarchy.h"
+
+namespace lilac::core
+{
+    std::string Replace(std::string str, const std::string& from, const std::string& to)
+    {
+        size_t start_pos;
+        while ((start_pos = str.find(from)) != std::string::npos)
+            str.replace(start_pos, from.length(), to);
+        return str;
+    }
+
+    void Hierarchy::ToString(std::stringstream& ss, const std::size_t indent) const
+    {
+        std::string indents(indent, '\t');
+
+        ss << indents << Kind << ',' << Name << ',' << ActualName;
+        if (Kind == HOK_Constant)
+        {
+            ss << ',' << Constant.Signed << ',';
+            if (Constant.Signed)
+                ss << Constant.ConstantSigned;
+            else
+                ss << Constant.ConstantUnsigned;
+        }
+
+        ss << '\n';
+
+        for (const auto& member: Members)
+            member.ToString(ss, indent + 1);
+    }
+
+    Hierarchy::Hierarchy(const HierarchyKind kind, std::string actualName, std::string name)
+        : ActualName(std::move(actualName)),
+          Name(std::move(name)),
+          Kind(kind),
+          Constant()
+    {
+    }
+
+    std::string Hierarchy::ToString() const
+    {
+        std::stringstream ss;
+        ToString(ss, 0);
+        return ss.str();
+    }
+
+    bool Hierarchy::operator==(const Hierarchy& obj) const
+    {
+        return ActualName == obj.ActualName;
+    }
+
+    bool Hierarchy::operator!=(const Hierarchy& obj) const
+    {
+        return ActualName != obj.ActualName;
+    }
+
+    bool Hierarchy::operator<(const Hierarchy& obj) const
+    {
+        return ActualName < obj.ActualName;
+    }
+}
