9019a53ca5f4061dd6d9b295e1280f55246addd6
Sharp0802 <sharp0802.business@gmail.com>
2024-05-14 22:28:40 +0900

    Refactor: Use new trailer system

--- lilac-cxx/src/enumvisitor.cxx
+++ lilac-cxx/src/enumvisitor.cxx
@@ -9,8 +9,8 @@ namespace lilac::cxx
         const auto type = decl->getIntegerType();
         const auto size = decl->getASTContext().getTypeSize(type) / 8;
 
-        m_Hierarchy.Constant.Signed = type->isSignedIntegerType();
-        m_Hierarchy.Constant.Size = size;
+        m_Hierarchy.GetConstantData().Signed = type->isSignedIntegerType();
+        m_Hierarchy.GetConstantData().Size = size;
     }
 
     bool Visitor<clang::EnumDecl>::VisitEnumConstantDecl(clang::EnumConstantDecl* decl)
@@ -20,11 +20,11 @@ namespace lilac::cxx
             decl->getNameAsString(),
             decl->getNameAsString()
         };
-        h.Constant = m_Hierarchy.Constant;
-        if (h.Constant.Signed)
-            h.Constant.ConstantSigned = decl->getValue().getSExtValue();
+        h.GetConstantData() = m_Hierarchy.GetConstantData();
+        if (h.GetConstantData().Signed)
+            h.GetConstantData().ConstantSigned = decl->getValue().getSExtValue();
         else
-            h.Constant.ConstantUnsigned = decl->getValue().getZExtValue();
+            h.GetConstantData().ConstantUnsigned = decl->getValue().getZExtValue();
         m_Hierarchy.Members.emplace(h);
 
         return true;
