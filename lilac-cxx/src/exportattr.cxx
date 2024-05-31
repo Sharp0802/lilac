36a58d3a96c985d9e070c8169434e4a363052a3a
Sharp0802 <sharp0802.business@gmail.com>
2024-05-31 09:16:41 +0900

    Feat: Get default visibility from language options

--- lilac-cxx/src/exportattr.cxx
+++ lilac-cxx/src/exportattr.cxx
@@ -1,55 +1,21 @@
 #include "pch.h"
 
-static bool ShouldExport(
-    clang::Sema&             sema,
-    const clang::ParsedAttr& attr,
-    const clang::NamedDecl*  decl)
-{
-    auto&      diag              = sema.getDiagnostics();
-    const auto invalidVisibility = diag.getCustomDiagID(
-        clang::DiagnosticsEngine::Warning,
-        "declaration cannot be exported because of its visibility; exporting skipped!");
-    const auto invalidAccess = diag.getCustomDiagID(
-        clang::DiagnosticsEngine::Warning,
-        "non-public declaration cannot be exported; exporting skipped!");
-
-    auto vis = decl->getExplicitVisibility(clang::isa<clang::RecordDecl>(decl)
-        ? clang::TypeDecl::VisibilityForType
-        : clang::TypeDecl::VisibilityForValue);
-
-    if (vis.has_value() && vis != clang::DefaultVisibility)
-    {
-        sema.Diag(attr.getLoc(), invalidVisibility);
-        return false;
-    }
-    // TODO: How to get default visibility (-fvisibility=?)
-
-    auto record = clang::cast_or_null<clang::CXXRecordDecl>(decl->getDeclContext());
-    if (!record)
-        return true;
-
-    auto access = clang::AS_none;
-    for (auto* member: record->decls())
-    {
-        if (clang::isa<clang::AccessSpecDecl>(member))
-            access = clang::cast<clang::AccessSpecDecl>(member)->getAccess();
-        if (clang::declaresSameEntity(member, decl))
-            break;
-    }
-
-    if (access == clang::AS_public ||
-        (record->isStruct() && access == clang::AS_none))
-        return true;
-
-    sema.Diag(attr.getLoc(), invalidAccess);
-    return false;
-}
-
 namespace
 {
     class ExportAttrInfo final : public clang::ParsedAttrInfo
     {
+        inline static clang::Visibility m_DefaultTypeVisibility = clang::DefaultVisibility;
+        inline static clang::Visibility m_DefaultValueVisibility = clang::DefaultVisibility;
+
     public:
+        static bool ShouldExport(
+            clang::Sema&             sema,
+            const clang::ParsedAttr& attr,
+            const clang::NamedDecl*  decl);
+
+        /************************************************************************
+         * derived from clang::ParsedAttrInfo                                   *
+         ************************************************************************/
         ExportAttrInfo();
 
         bool diagAppertainsToDecl(
@@ -62,10 +28,63 @@ namespace
             clang::Decl*             decl,
             const clang::ParsedAttr& attr) const override;
 
-
+        bool acceptsLangOpts(
+            const clang::LangOptions& LO) const override;
     };
 
 #define ATTR_NAME "apiexport"
+
+    bool ExportAttrInfo::ShouldExport(clang::Sema& sema, const clang::ParsedAttr& attr, const clang::NamedDecl* decl)
+    {
+        /************************************************************************
+         * Initialize diagnostics                                               *
+         ************************************************************************/
+        auto&      diag              = sema.getDiagnostics();
+        const auto invalidVisibility = diag.getCustomDiagID(
+            clang::DiagnosticsEngine::Warning,
+            "declaration cannot be exported because of its visibility; exporting skipped!");
+        const auto invalidAccess = diag.getCustomDiagID(
+            clang::DiagnosticsEngine::Warning,
+            "non-public declaration cannot be exported; exporting skipped!");
+
+        /************************************************************************
+         * Ensure visibility of declaration to default                          *
+         ************************************************************************/
+        if (const auto vis = decl->getExplicitVisibility(clang::isa<clang::RecordDecl>(decl)
+                ? clang::TypeDecl::VisibilityForType
+                : clang::TypeDecl::VisibilityForValue);
+            (vis.has_value() && vis != clang::DefaultVisibility) ||
+            (clang::isa<clang::RecordDecl>(decl)
+                ? m_DefaultTypeVisibility
+                : m_DefaultValueVisibility) != clang::DefaultVisibility)
+        {
+            sema.Diag(attr.getLoc(), invalidVisibility);
+            return false;
+        }
+
+        /************************************************************************
+         * Check access modifiers                                               *
+         ************************************************************************/
+        auto record = clang::cast_or_null<clang::CXXRecordDecl>(decl->getDeclContext());
+        if (!record)
+            return true;
+
+        auto access = clang::AS_none;
+        for (auto* member: record->decls())
+        {
+            if (clang::isa<clang::AccessSpecDecl>(member))
+                access = clang::cast<clang::AccessSpecDecl>(member)->getAccess();
+            if (clang::declaresSameEntity(member, decl))
+                break;
+        }
+
+        if (access == clang::AS_public ||
+            (record->isStruct() && access == clang::AS_none))
+            return true;
+
+        sema.Diag(attr.getLoc(), invalidAccess);
+        return false;
+    }
 
     ExportAttrInfo::ExportAttrInfo()
     {
@@ -87,6 +106,9 @@ namespace
         const clang::ParsedAttr& attr,
         const clang::Decl*       decl) const
     {
+        /************************************************************************
+         * Ensure whether the declaration can be exported                       *
+         ************************************************************************/
         if (!clang::isa<clang::FunctionDecl>(decl) &&
             !clang::isa<clang::MSPropertyDecl>(decl) &&
             !clang::isa<clang::RecordDecl>(decl))
@@ -98,7 +120,6 @@ namespace
             sema.Diag(attr.getLoc(), invalidDeclType) << attr;
             return false;
         }
-
         if (!ShouldExport(sema, attr, clang::cast<clang::NamedDecl>(decl)))
             return false;
 
@@ -122,6 +143,13 @@ namespace
         // TODO
 
         return AttributeApplied;
+    }
+
+    bool ExportAttrInfo::acceptsLangOpts(const clang::LangOptions& LO) const
+    {
+        m_DefaultTypeVisibility  = LO.getTypeVisibilityMode();
+        m_DefaultValueVisibility = LO.getValueVisibilityMode();
+        return true;
     }
 }
 
