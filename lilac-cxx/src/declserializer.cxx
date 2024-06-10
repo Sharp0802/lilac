8f22f05ede37fb3a91365413c2a09c117f6fbec3
Sharp0802 <sharp0802.business@gmail.com>
2024-06-10 16:22:21 +0900

    Feat: Create name mangling helpers

---
+++ lilac-cxx/src/declserializer.cxx
@@ -0,0 +1,80 @@
+#include "declserializer.h"
+
+namespace lilac::cxx
+{
+    bool EnsureExporting(clang::Sema& sema, clang::NamedDecl* decl)
+    {
+        const auto diag = sema.getDiagnostics().getCustomDiagID(
+            clang::DiagnosticsEngine::Error,
+            "not-exported function '%0' cannot be serialized");
+
+        if (ShouldExport(sema, decl))
+            return true;
+
+        sema.Diag(decl->getLocation(), diag) << decl;
+        return false;
+    }
+
+    [[nodiscard]]
+    std::string GetTypeString(const clang::TagDecl* decl);
+
+    std::string GetName(const clang::NamedDecl* decl)
+    {
+        if (auto* tag = clang::dyn_cast<clang::TagDecl>(decl))
+            return GetTypeString(tag);
+
+        clang::ASTNameGenerator gen(decl->getASTContext());
+        return gen.getName(decl);
+    }
+
+    std::string GetDisplayName(const clang::NamedDecl* decl)
+    {
+        return decl->getNameAsString();
+    }
+
+    std::string GetTypeString(const clang::TagDecl* decl)
+    {
+        static std::map<clang::TagTypeKind, std::string> map = {
+            { clang::TagTypeKind::Struct, "struct" },
+            { clang::TagTypeKind::Interface, "interface" },
+            { clang::TagTypeKind::Union, "union" },
+            { clang::TagTypeKind::Class, "class" },
+            { clang::TagTypeKind::Enum, "enum" }
+        };
+
+        const clang::DeclContext* ctx = decl;
+        std::stack<std::string>   stk;
+        do
+        {
+            stk.emplace(decl->getName());
+            ctx = ctx->getParent();
+        } while (!ctx->isTranslationUnit());
+
+        std::stringstream ss;
+        ss << map[decl->getTagKind()];
+        for (; !stk.empty(); stk.pop())
+            ss << '.' << stk.top();
+
+        return ss.str();
+    }
+
+    std::string GetTypeString(clang::QualType type)
+    {
+        type = type.getCanonicalType();
+
+        std::size_t ref = 0;
+        for (; type->isPointerType(); ref++, type = type->getPointeeType().getCanonicalType())
+        {
+        }
+
+        return GetTypeString(type->getAsTagDecl()) + std::string(ref, '*');
+    }
+
+    clang::FunctionDecl* FindFunction(const clang::CXXRecordDecl* decl, const std::string& name)
+    {
+        for (const auto method: decl->methods())
+            if (method->getName() == name)
+                return method;
+        return nullptr;
+    }
+}
