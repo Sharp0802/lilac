d34e2b3e9b5ea10cbfa556fb4b2e3bddabee7822
Sharp0802 <sharp0802.business@gmail.com>
2024-08-11 21:40:59 +0900

    :fire: Remove old codebase

--- lilac-nm/pch.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <fstream>
-#include <llvm/IRReader/IRReader.h>
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/Module.h>
-#include <llvm/Support/CommandLine.h>
-#include <clang/Tooling/JSONCompilationDatabase.h>
-#include <clang/Tooling/Tooling.h>
