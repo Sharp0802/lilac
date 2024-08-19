7ca8dba1e2aa973768cc2dff43fc3e9ac74fe67c
Sharp0802 <sharp0802.business@gmail.com>
2024-08-19 21:40:37 +0900

    :construction: Create project for C# backend

---
+++ langs/csharp/backend/pch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/********************************
+ * External Dependencies        *
+ ********************************/
+#include <frxml.h>
+
+/********************************
+ * Standard Libraries           *
+ ********************************/
+#include <algorithm>
+#include <fstream>
+#include <utility>
+#include <memory>
+#include <ranges>
+#include <sstream>
+#include <stack>
+#include <variant>
+#include <vector>
