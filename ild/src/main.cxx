dc1c1b6581bb5591a609e07d5e0c0014f5f8008f
Sharp0802 <sharp0802.business@gmail.com>
2024-08-21 12:20:24 +0900

    :page_facing_up: Update license header for source codes

--- ild/src/main.cxx
+++ ild/src/main.cxx
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2024  Yeong-won Seo
+ *
+ * This file is part of LILAC.
+ *
+ * LILAC is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3, or (at your option) any later
+ * version.
+ *
+ * LILAC is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "pch.h"
 
 #include "csharp/module.h"
@@ -41,7 +60,7 @@ llvm::cl::opt<std::string> OutPathOpt(
 
 int main(int argc, const char* argv[])
 {
-    llvm::cl::SetVersionPrinter([] (llvm::raw_ostream& ost)
+    llvm::cl::SetVersionPrinter([](llvm::raw_ostream& ost)
     {
         ost << "ild (ILD) " << LILAC_DATE << '\n' << LILAC_SHORT_LICENSE;
     });
@@ -51,7 +70,8 @@ int main(int argc, const char* argv[])
     std::function<int(const std::string&, const std::string&, const std::string&)> caller;
     switch (Module.getValue())
     {
-        case CSharp: caller = MODULE_REF(csharp); break;
+        case CSharp: caller = MODULE_REF(csharp);
+            break;
     }
 
     return caller(XMLPathOpt.getValue(), LibPathOpt.getValue(), OutPathOpt.getValue());
