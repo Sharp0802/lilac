263f38f04d15e4a703f7345449071b6e0b3d3497
Sharp0802 <sharp0802.business@gmail.com>
2024-08-21 23:02:12 +0900

    :building_construction: Store class at `.data` section rather than use module initializer function

---
+++ langs/shared/include/shared/backend.h
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2024  Yeong-won Seo
+ *
+ * This file is part of LILAC.
+ *
+ * LILAC is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3, or (at your option) any later
+ * version.
+ *
+ * LILAC is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace lilac::shared
+{
+    enum ModuleKind
+    {
+        CSharp
+    };
+
+    /**
+     * @brief An action that generates language-specific bridge of given interface representation
+     */
+    class BackendAction
+    {
+        const std::string m_Name;
+        const std::string m_Desc;
+        ModuleKind        m_Kind;
+
+    public:
+        BackendAction(ModuleKind kind, std::string name, std::string desc);
+
+        virtual ~BackendAction() = default;
+
+        /**
+         * @brief Gets the name of this action
+         * @return The name of this action
+         */
+        [[nodiscard]]
+        const std::string& Name() const;
+
+        /**
+         * @brief Gets the description of this action
+         * @return The description of this action
+         */
+        [[nodiscard]]
+        const std::string& Desc() const;
+
+        /**
+         * @brief Gets the kind of this action
+         * @return The kind of this action
+         */
+        [[nodiscard]]
+        ModuleKind Kind() const;
+
+        /**
+         * @brief Runs action with given paths
+         *
+         * @param confPath A path of XML source that contains interface representation data
+         * @param libPath A path of bridge used by target language of the action
+         * @param outPath A path to place output
+         * @return Zero if action completed successfully; otherwise, non-zero returned
+         */
+        [[nodiscard]]
+        virtual int Run(
+            std::string confPath,
+            std::string libPath,
+            std::string outPath) const = 0;
+
+        /**
+         * @brief Gets all registered actions
+         * @return All registered actions
+         */
+        static const std::vector<BackendAction*>& GetRegistered();
+    };
+}
--- langs/shared/src/backend.cxx
+++ langs/shared/src/backend.cxx
@@ -17,8 +17,36 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#pragma once
-
 #include "shared/backend.h"
 
-MODULE(csharp);
+#include <utility>
+#include <vector>
+
+static std::vector<lilac::shared::BackendAction*> backends;
+
+
+lilac::shared::BackendAction::BackendAction(ModuleKind kind, std::string name, std::string desc)
+    : m_Name(std::move(name)), m_Desc(std::move(desc)), m_Kind(kind)
+{
+    backends.push_back(this);
+}
+
+const std::string& lilac::shared::BackendAction::Name() const
+{
+    return m_Name;
+}
+
+const std::string& lilac::shared::BackendAction::Desc() const
+{
+    return m_Desc;
+}
+
+lilac::shared::ModuleKind lilac::shared::BackendAction::Kind() const
+{
+    return m_Kind;
+}
+
+const std::vector<lilac::shared::BackendAction*>& lilac::shared::BackendAction::GetRegistered()
+{
+    return backends;
+}
