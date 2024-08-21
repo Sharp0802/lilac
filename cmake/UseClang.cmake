bdf4cdca16ae6354b9620aa29db5116417fe6e67
Sharp0802 <sharp0802.business@gmail.com>
2024-08-21 16:55:46 +0900

    :hammer: Override compiler only when compiler is not LLVM/Clang

--- cmake/UseClang.cmake
+++ cmake/UseClang.cmake
@@ -1,12 +1,13 @@
+if (NOT CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    execute_process(
+            COMMAND $ENV{SHELL} -c "which clang"
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            OUTPUT_VARIABLE CLANG_C_EXE)
+    execute_process(
+            COMMAND $ENV{SHELL} -c "which clang++"
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            OUTPUT_VARIABLE CLANG_CXX_EXE)
 
-execute_process(
-        COMMAND $ENV{SHELL} -c "which clang"
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        OUTPUT_VARIABLE CLANG_C_EXE)
-execute_process(
-        COMMAND $ENV{SHELL} -c "which clang++"
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        OUTPUT_VARIABLE CLANG_CXX_EXE)
-
-set(CMAKE_C_COMPILER "${CLANG_C_EXE}")
-set(CMAKE_CXX_COMPILER "${CLANG_CXX_EXE}")
+    set(CMAKE_C_COMPILER "${CLANG_C_EXE}")
+    set(CMAKE_CXX_COMPILER "${CLANG_CXX_EXE}")
+endif ()
